5e64659186c9dbc49256a6976f19b640e8c1735a
rakshit-23 <110079890+rakshit-23@users.noreply.github.com>
2022-12-09 16:13:35 +0530

    Update user_proj_example.v

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -74,92 +74,48 @@ module user_proj_example #(
     wire [`MPRJ_IO_PADS-1:0] io_in;
     wire [`MPRJ_IO_PADS-1:0] io_out;
     wire [`MPRJ_IO_PADS-1:0] io_oeb;
-
-    wire [31:0] rdata; 
-    wire [31:0] wdata;
-    wire [BITS-1:0] count;
-
-    wire valid;
-    wire [3:0] wstrb;
-    wire [31:0] la_write;
-
-    // WB MI A
-    assign valid = wbs_cyc_i && wbs_stb_i; 
-    assign wstrb = wbs_sel_i & {4{wbs_we_i}};
-    assign wbs_dat_o = rdata;
-    assign wdata = wbs_dat_i;
+    wire [7:0] data_in,data_out;
+    wire sl_ser,sr_ser;
+    wire [1:0] select;
+    iiitb_usr usr1(data_in,data_out,clk,rst,select,sl_ser,sr_ser);
 
     // IO
-    assign io_out = count;
-    assign io_oeb = {(`MPRJ_IO_PADS-1){rst}};
+    assign clk = wb_clk_i;
+    assign rst = wb_rst_i;
+    assign {data_in,select,sl_ser,sr_ser}= io_in[37:26];
+    assign io_out[37:30] = data_out;
+    assign io_oeb = 0;
 
     // IRQ
     assign irq = 3'b000;	// Unused
 
-    // LA
-    assign la_data_out = {{(127-BITS){1'b0}}, count};
-    // Assuming LA probes [63:32] are for controlling the count register  
-    assign la_write = ~la_oenb[63:32] & ~{BITS{valid}};
-    // Assuming LA probes [65:64] are for controlling the count clk & reset  
-    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
-    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
-
-    counter #(
-        .BITS(BITS)
-    ) counter(
-        .clk(clk),
-        .reset(rst),
-        .ready(wbs_ack_o),
-        .valid(valid),
-        .rdata(rdata),
-        .wdata(wbs_dat_i),
-        .wstrb(wstrb),
-        .la_write(la_write),
-        .la_input(la_data_in[63:32]),
-        .count(count)
-    );
+    
 
 endmodule
 
-module counter #(
-    parameter BITS = 32
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output ready,
-    output [BITS-1:0] rdata,
-    output [BITS-1:0] count
-);
-    reg ready;
-    reg [BITS-1:0] count;
-    reg [BITS-1:0] rdata;
-
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 0;
-            ready <= 0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-                if (wstrb[2]) count[23:16] <= wdata[23:16];
-                if (wstrb[3]) count[31:24] <= wdata[31:24];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
-            end
-        end
+    module iiitb_usr  (data_in,data_out,clock,reset,select,sl_ser,sr_ser);
+    input [7:0] data_in;
+    output reg [7:0] data_out;
+    input reset,clock,sl_ser,sr_ser;
+    input [1:0] select;
+    
+    always@(posedge clock)
+    begin
+        if(reset==1'b1)
+            data_out<=8'b00000000;
+        else
+        begin 
+            if(select==2'b00)   //Shift left
+                data_out<={data_out[6:0],sl_ser};
+            else if(select==2'b01)  //Shift right
+                data_out<={sr_ser,data_out[7:1]}; 
+            else if(select==2'b10)  // Parallel load
+                data_out<=data_in;
+            else
+                data_out<=data_out; //Temporary storage
+         end
     end
-
+    
 endmodule
+
 `default_nettype wire
